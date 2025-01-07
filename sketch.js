3df0e8f498a2a564d473f87f78ac329c7e5a40e5
RowanWA <Rowan.Armitage@student.norwichuni.ac.uk>
2025-01-07 14:02:28 +0000

    1

--- sketch.js
+++ sketch.js
@@ -5,23 +5,26 @@ https://data.giss.nasa.gov/gistemp/
 const HEADERTEXT = "Annual Global Temperature Fluctuation 1880 - 2023";
 const TOPMARGIN = 52;
 let averageTemps;
+let currentYear;
 let minTemp, maxTemp;
 let hotColour, coldColour;
 let vhc = 0;
 // how much to rotate the circle by each frame
 let rot = 0;
+
 function preload() {
-  table = loadTable("data/data.csv", "csv", "header");
+  table = loadTable("data/2010-2019.csv/data.csv", "csv", "header");
 }
 function setup() {
   createCanvas(innerWidth, innerHeight);
   averageTemps = table.getColumn(13) // get the variation from the average temp for 1880 to 2023 
+  currentYear = table.getColumn(1)
   // find the lowest value
   minTemp = min(averageTemps)
   // find the highest value
   maxTemp = max(averageTemps)
   // define 2 colours that will form either end of a range of possible colurs
-  hotColour = color(vhc, 0, 0)
+  hotColour = color(255, 0, 0)
   coldColour = color(0, 0, 255)
   // use squared ends when draing strokes
   strokeCap(SQUARE)
@@ -30,18 +33,26 @@ function setup() {
   // start Midi
   setupController();
 }
+
+const temps = [averageTemps];
+
+
 function draw() {
+  let r = height * 0.25
   background(0);
   textSize(36);
-  fill(255)
+  stroke(255);
+  fill(0)
+  circle(width * 0.5, height * 0.5, r * 2);
   noStroke();
+  fill(255)
   // draw header text
   text(HEADERTEXT, width/2, TOPMARGIN/2);
   // the thickness of each line drawn
   strokeWeight(10)
   // draw a circle
   // define radius
-  let r = height * 0.25
+  // let r = height * 0.25
   // coords of each point
   let x,y;
   // an index used to access the relevant temp data
@@ -50,21 +61,24 @@ function draw() {
   let len = 100;
   // how much to advance rotation of the circle by each frame
   let rotInc = 0.0025;
+  fill(lerpColor(coldColour, hotColour, 0))
+  
   // we want a circle with a number of points that matches the temp data
   // loop from 0 to TAU using the available data to define an increment to the angle theta ie  +=TAU/(averageTemps.length-1)
   // however, because we are progressively rotating the circle
   // we use an offset rot to control the start and end of the loop
   for(let theta = rot; theta < TAU + rot; theta += TAU/(averageTemps.length-1)){
-    // use trig to calculate Cartesian coords of circle 
-    x = cos(theta) * r + width/2;
-    y = sin(theta) * r + height/2;
-    // calculate a value from 0 to 1 based on the current temp value compared to precalculated min and max temp values
-    let delta = map(averageTemps[i], minTemp, maxTemp, 0, 1)
-    // use lerpColour to derive a colour value proportionally between cold and hot colours
-    stroke(lerpColor(coldColour, hotColour, delta))
-    // extend a line from the edge of the circle inwards/outwards at a length relating to the temp data
-    line(x, y, x + cos(theta) * len * averageTemps[i], y + sin(theta) * len * averageTemps[i])
-    // increment the index used to access the correct temp data
+
+  //   // calculate a value from 0 to 1 based on the current temp value compared to precalculated min and max temp values
+    let delta = (averageTemps[1] * r)
+  //   // use trig to calculate Cartesian coords of circle 
+    x = cos(theta) * delta + width/2;
+    y = sin(theta) * delta + height/2;
+  //   // use lerpColour to derive a colour value proportionally between cold and hot colours
+    // stroke(lerpColor(coldColour, hotColour, delta))
+  //   // extend a line from the edge of the circle inwards/outwards at a length relating to the temp data
+    circle(x, y, 20, 20)
+  //   // increment the index used to access the correct temp data
     i++;
   }
   // once the circle has been rendered, increment the rotation value
