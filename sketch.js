70bc123e44e913d851f0c350a6000dcb08bdcaaa
RowanWA <Rowan.Armitage@student.norwichuni.ac.uk>
2025-01-07 15:33:19 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,6 +11,8 @@ let hotColour, coldColour;
 let vhc = 0;
 // how much to rotate the circle by each frame
 let rot = 0;
+let theta = [];
+let radius = [];
 
 function preload() {
   table = loadTable("data/data.csv", "csv", "header");
@@ -32,6 +34,12 @@ function setup() {
   textAlign(CENTER, CENTER);
   // start Midi
   setupController();
+  let r = height * 0.25
+  for (let i = 0; i < 10; i++) {
+    radius.push(averageTemps[i] * r)
+    theta.push((2 * Math.PI / 10) * i)
+  }
+
 }
 
 const temps = [averageTemps];
@@ -41,15 +49,18 @@ function draw() {
   let r = height * 0.25
   background(0);
   textSize(36);
-  stroke(255);
-  fill(0)
-  circle(width * 0.5, height * 0.5, r * 2);
-  noStroke();
   fill(255)
-  // draw header text
+  noStroke();
   text(HEADERTEXT, width / 2, TOPMARGIN / 2);
-  // the thickness of each line drawn
+  translate(width / 2, height / 2);
+  fill(0)
+  stroke(255);
   strokeWeight(10)
+  
+  circle(0, 0, r * 2);
+  // draw header text
+  // the thickness of each line drawn
+  noStroke();
   // draw a circle
   // define radius
   // let r = height * 0.25
@@ -63,27 +74,39 @@ function draw() {
   let rotInc = 0.0025;
   fill(lerpColor(coldColour, hotColour, 0))
 
+
   // we want a circle with a number of points that matches the temp data
   // loop from 0 to TAU using the available data to define an increment to the angle theta ie  +=TAU/(averageTemps.length-1)
   // however, because we are progressively rotating the circle
   // we use an offset rot to control the start and end of the loop
-  for (let theta = rot; theta < TAU + rot; theta += TAU / (1)) {
-    for (let j = 0; j < 10; j++) {
-      setTimeout(function () {
-      //   // calculate a value from 0 to 1 based on the current temp value compared to precalculated min and max temp values
-      let delta = (averageTemps[j+1] * r)
-      //   // use trig to calculate Cartesian coords of circle 
-      x = cos(theta) * delta + width / 2;
-      y = sin(theta) * delta + height / 2;
-      //   // use lerpColour to derive a colour value proportionally between cold and hot colours
-      // stroke(lerpColor(coldColour, hotColour, delta))
-      //   // extend a line from the edge of the circle inwards/outwards at a length relating to the temp data
-      circle(x, y, 20, 20)
-      //   // increment the index used to access the correct temp data
-      }, j * 3000);
-
-    }
+  // for (let theta = rot; theta < TAU + rot; theta += TAU / (1)) {
+    
+  //   for (let i = 0; i < 10; i++) {
+  //     let xValues = [cos(theta) * (averageTemps[i] * r) + width / 2];
+  //   }
+  //   for (let l = 0; l < 10; l++) {
+  //   let yValues = [sin(theta) * (averageTemps[l] * r) + height / 2];
+  //   }
+  //   for (let j = 0; j < 10; j++) {
+  //     setTimeout(function () {
+  //     //   // calculate a value from 0 tod on the current temp value compared to precalculated min and max temp values
+      
+  //     // x = cos(theta) * delta + width / 2;
+  //     // y = sin(theta) * delta + height / 2;
+  //     //   
+  //     circle(xValues[j], yValues[l], 20, 20)
+  //     //   creates circles for data points
+  //     }, j * 500);
+  //   }
+  // }
+  for (let i = 0; i < 10; i++) {
+    x = cos(theta[i] + rot) * radius[i];
+    y = sin(theta[i] + rot) * radius[i];
+    circle(x, y, 20, 20)
+  //     //   creates circles for data points
   }
+
+
   // once the circle has been rendered, increment the rotation value
   // no need to return rot to zero once TAU has been reached 
   // as the effective value will be the remainder when divided by TAU
