59ca08ade8d1738fd8b7fbfd3094ede5ac539480
RowanWA <Rowan.Armitage@student.norwichuni.ac.uk>
2025-01-10 14:17:50 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,14 +4,13 @@ https://data.giss.nasa.gov/gistemp/
 */
 const HEADERTEXT = "Annual Global Temperature Fluctuation 1980 - 2019";
 const TOPMARGIN = 52;
-const decadeTimeout = setTimeout(nextDecade, 100000);
-const yearTimeout = setTimeout(nextYear, 10000);
+let yearTimeout = setTimeout(nextYear, 10000);
 let averageTemps = [];
 // let currentMonth;
 let minTemp, maxTemp;
 let hotColour, coldColour;
 let vhc = 0;
-let exactYear = 0
+let exactYear = 0;
 // how much to rotate the circle by each frame
 let rot = 0;
 let trot = 0;
@@ -21,9 +20,7 @@ let radius = [];
 let delta = [];
 let textRadius = [];
 let stopDraw = 0;
-
-
-
+let multipleSelect = 1;
 let months = [
   "Jan",
   "Feb",
@@ -41,14 +38,14 @@ let months = [
 
 //label for circles
 let tempDegree = [
-  "0°",
+  "0°"
 ];
 
 //time period labels to be displayed
 let timePeriods = [
-  "1980 - 1989",
-  "1990 - 1999",
-  "2000 - 2009",
+  "1980 - 1989", 
+  "1990 - 1999", 
+  "2000 - 2009", 
   "2010 - 2019",
 ];
 
@@ -63,8 +60,8 @@ let yearEighties = [
   "1986",
   "1987",
   "1988",
-  "1989"
-]
+  "1989",
+];
 
 //time labels for 1990s
 let yearNineties = [
@@ -77,8 +74,8 @@ let yearNineties = [
   "1996",
   "1997",
   "1998",
-  "1999"
-]
+  "1999",
+];
 
 //time labels for 2000s
 let yearNoughties = [
@@ -92,7 +89,7 @@ let yearNoughties = [
   "2007",
   "2008",
   "2009",
-]
+];
 
 //time labels for 2010s
 let yearTens = [
@@ -105,8 +102,8 @@ let yearTens = [
   "2016",
   "2017",
   "2018",
-  "2019"
-]
+  "2019",
+];
 
 let decadeSelection = yearEighties;
 
@@ -122,14 +119,14 @@ let circleSpeed = 0.0025;
 let decadeButton = 0;
 let yearDial = 0;
 // distance of where the circles place is expanded
-let circleDistance = 0.25
+let circleDistance = 0.25;
 
 function preload() {
   table = loadTable("data/data.csv", "csv", "header");
 }
 
 function setup() {
-  createCanvas(innerWidth, innerHeight);
+  createCanvas(innerWidth * 0.98, innerHeight * 0.9);
   for (let rowI = 0; rowI < 40; rowI++) {
     for (let colI = 0; colI < 12; colI++) {
       averageTemps.push(table.get(rowI, colI));
@@ -143,7 +140,7 @@ function setup() {
   hotColour = color(255, 0, 0, a);
   coldColour = color(0, 0, 255, a);
   // use squared ends when draing strokes
-  strokeCap(SQUARE)
+  strokeCap(SQUARE);
   // centre text
   textAlign(CENTER, CENTER);
   // start Midi
@@ -157,17 +154,17 @@ function setup() {
 //Calculate function
 function calculate() {
   let decade = decadeButton;
-  r = (height * circleDistance)
-  textR = height * 0.25
-  radius = []
-  textRadius = []
-  theta = []
+  r = height * circleDistance;
+  textR = height * 0.25;
+  radius = [];
+  textRadius = [];
+  theta = [];
   delta = [];
-  for (let i = (0 + (120 * decade)); i < (120 + (120 * decade)); i++) {
-    radius.push(averageTemps[i] * r)
-    textRadius.push(textR + ((r * 0.60)))
-    theta.push(((2 * Math.PI / 12) * i) + (0.25 * (Math.random() - 0.5)))
-    thetaT.push(((2 * Math.PI / 12) * i))
+  for (let i = 0 + 120 * decade; i < 120 + 120 * decade; i++) {
+    radius.push(averageTemps[i] * r);
+    textRadius.push(textR + r * 0.6);
+    theta.push(((2 * Math.PI) / 12) * i + 0.25 * (Math.random() - 0.5));
+    thetaT.push(((2 * Math.PI) / 12) * i);
     delta.push(map(averageTemps[i], minTemp, maxTemp, 0, 1));
   }
 }
@@ -175,41 +172,41 @@ function calculate() {
 function draw() {
   background(0, 15);
 
-  textSize(36);
-  fill(255)
+  textSize(40);
+  fill(255);
   noStroke();
   text(HEADERTEXT, width / 2, TOPMARGIN / 2);
-  fill(0)
+  fill(0);
 
   //time periods text
-  fill(255)
-  text(timePeriods[0], 140, 150)
-  text(timePeriods[1], 140, 190)
-  text(timePeriods[2], 140, 230)
-  text(timePeriods[3], 140, 270)
-  stroke(255)
-  strokeWeight(3)
-  line(40, 167 + (40 * decadeButton), 245, 167 + (40 * decadeButton))
+  fill(255);
+  text(timePeriods[0], width * 0.15, 150);
+  text(timePeriods[1], width * 0.15, 200);
+  text(timePeriods[2], width * 0.15, 250);
+  text(timePeriods[3], width * 0.15, 300);
+  stroke(255);
+  strokeWeight(3);
+  line(width * 0.121, 172 + 48 * decadeButton, width * 0.179, 172 + 48 * decadeButton);
 
   //exact year text
-  fill(255)
-  stroke(255)
-  textSize(50)
-  strokeWeight(1)
-  text(decadeSelection[exactYear], 1200, 800);
+  fill(255);
+  stroke(255);
+  textSize(50);
+  strokeWeight(1);
+  text(decadeSelection[exactYear], width * 0.8, height * 0.8);
 
   translate(width / 2, height / 2);
 
   // outer ring
-  fill(0, 0, 0, 0)
+  fill(0, 0, 0, 0);
   stroke(255);
-  strokeWeight(1)
-  circle(0, 0, r * 2.80);
+  strokeWeight(4);
+  circle(0, 0, r * 2.8);
 
   // middle ring
-  fill(0, 0, 0, 0)
+  fill(0, 0, 0, 0);
   stroke(255);
-  strokeWeight(1)
+  strokeWeight(4);
   circle(0, 0, r);
 
   noStroke();
@@ -221,9 +218,10 @@ function draw() {
   //stroke for circles
   strokeWeight(1);
 
+  //
   for (let i = 0; i < 120; i++) {
-    fill(lerpColor(coldColour, hotColour, delta[i], a))
-    stroke(lerpColor(coldColour, hotColour, delta[i], a))
+    fill(lerpColor(coldColour, hotColour, delta[i], a));
+    stroke(lerpColor(coldColour, hotColour, delta[i], a));
     push();
     x = cos(theta[i] + rot) * radius[i] + randomGaussian(1, gaussianRandom);
     y = sin(theta[i] + rot) * radius[i] + randomGaussian(1, gaussianRandom);
@@ -231,16 +229,16 @@ function draw() {
     ty = sin(thetaT[i] + rot) * textRadius[i];
 
     //Draws points
-    circle(x, y, circleSize)
-    ellipse(x, y)
-    stroke(0)
-    strokeWeight(1)
-    fill(225)
+    circle(x, y, 2 * circleSize);
+    ellipse(x, y);
+    stroke(0);
+    strokeWeight(1);
+    fill(225);
 
     //draws months and zero marker
-    text(months[i], tx, ty)
-    textSize(20)
-    text(tempDegree[i], tx / 2.8, ty / 2.8)
+    text(months[i], tx, ty);
+    textSize(28);
+    text(tempDegree[i], tx / 2.8, ty / 2.8);
 
     //
     pop();
@@ -252,76 +250,81 @@ function draw() {
     tx = cos(thetaT[i] + rot) * textRadius[i];
     ty = sin(thetaT[i] + rot) * textRadius[i];
 
-    stroke(0)
-    strokeWeight(1)
-    fill(225)
-    textSize(20)
-    text(tempDegree[i], tx / 2.8, ty / 2.8)
+    stroke(0);
+    strokeWeight(1);
+    fill(225);
+    textSize(28);
+    text(tempDegree[i], tx / 2.8, ty / 2.8);
 
     pop();
   }
 
-  //
-  for (let i = 0 + (12 * (yearDial * mutipleSelect)); i < 12 + (12 * yearDial); i++) {
-    fill(0, 0, 0, 0)
-    stroke(255, 255, 255, 200)
+  
+  for (let i = 0 + (12 * (yearDial * multipleSelect)); i < 12 + (12 * yearDial); i++) {
+    fill(0, 0, 0, 0);
+    stroke(255, 255, 255, 200);
     x = cos(theta[i] + rot) * radius[i] + randomGaussian(1, gaussianRandom);
     y = sin(theta[i] + rot) * radius[i] + randomGaussian(1, gaussianRandom);
 
     //Draw circle
-    circle(x, y, circleSize)
-    stroke(0)
-    strokeWeight(1)
-    fill(225)
+    circle(x, y, 2 * circleSize);
+    stroke(0);
+    strokeWeight(1);
+    fill(225);
   }
 
   // once the circle has been rendered, increment the rotation value
-  // no need to return rot to zero once TAU has been reached 
+  // no need to return rot to zero once TAU has been reached
   // as the effective value will be the remainder when divided by TAU
   rot += rotInc;
 }
 
+function compareArrays(a, b) {
+
+}
+
+
 function nextDecade() {
   if (decadeButton < 3) { 
   decadeButton = (decadeButton + 1);
-  } else if (decadeButton = 3) {
+  } else {
     decadeButton = (0);
   }
-  if (decadeSelection = yearTens) {
+  if (decadeSelection == yearTens) {
     decadeSelection = yearEighties;
-  } else if (decadeSelection = yearNoughties) {
+  } else if (decadeSelection == yearNoughties) {
     decadeSelection = yearTens;
-  } else if (decadeSelection = yearNineties) {
+  } else if (decadeSelection == yearNineties) {
     decadeSelection = yearNoughties;
-  } else if (decadeSelection = yearEighties) {
+  } else if (decadeSelection == yearEighties) {
     decadeSelection = yearNineties;
   }
-  decadeTimeout = setTimeout(nextDecade, 100000);
+  calculate();
 }
 
 function nextYear() {
   if (yearDial < 9) { 
   yearDial = (yearDial + 1);
-  } else if (yearDial = 9) {
+  } else if (yearDial == 9) {
     yearDial = (0);
   }
   if (exactYear < 9) { 
     exactYear = (exactYear + 1);
-    } else if (exactYear = 9) {
+    } else if (exactYear == 9) {
       exactYear = (0);
+      nextDecade();
     }
+    clearTimeout(yearTimeout);
   yearTimeout = setTimeout(nextYear, 10000);
 }
 
 /**
  * React to inputs from the control change sliders in the Midi controller
- * @param {Event} e 
+ * @param {Event} e
  */
 function allCC(e) {
-  console.log('controller:', e.controller.number, 'value:', e.value);
-  clearTimeout(decadeTimeout);
+  console.log("controller:", e.controller.number, "value:", e.value);
   clearTimeout(yearTimeout);
-  decadeTimeout = setTimeout(nextDecade, 100000);
   yearTimeout = setTimeout(nextYear, 10000);
   switch (e.controller.number) {
     case 32: {
@@ -330,7 +333,7 @@ function allCC(e) {
       break;
     }
     case 33: {
-      mutipleSelect = Math.trunc(e.value);
+      multipleSelect = -((Math.trunc  (e.value))-1);
       break;
     }
     case 34: {
@@ -341,7 +344,7 @@ function allCC(e) {
     }
     case 36: {
       //slider 1
-      // Change the alpha 
+      // Change the alpha
       a = 255 * e.value + 20; // + 50 so that opacity never goes to 0
       // define 2 colours that will form either end of a range of possible colurs
       hotColour = color(255, 0, 0, a);
@@ -374,13 +377,11 @@ function allCC(e) {
 /**
  * All buttons change between four different time perio
  * React to inputs from the bottom buttons on the controller
- * @param {Event} e 
+ * @param {Event} e
  */
 function allNoteOn(e) {
-  console.log('controller:', e.data[1], 'value:', e.value);
-  clearTimeout(decadeTimeout);
+  console.log("controller:", e.data[1], "value:", e.value);
   clearTimeout(yearTimeout);
-  decadeTimeout = setTimeout(nextDecade, 100000);
   yearTimeout = setTimeout(nextYear, 10000);
   switch (e.data[1]) {
     case 40: {
